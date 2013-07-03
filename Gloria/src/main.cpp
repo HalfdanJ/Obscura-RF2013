9c618e25562e8b4b39b91a8049a1c2a07940a886
Johan Bichel Lindegaard <johan@johan.cc>
2013-07-03 14:33:16 +0200

    Larger window

--- Gloria/src/main.cpp
+++ Gloria/src/main.cpp
@@ -5,6 +5,6 @@
 int main(){
 	ofAppGlutWindow window; // create a window
 	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(&window, 1200, 600, OF_WINDOW);
+	ofSetupOpenGL(&window, 1600, 800, OF_WINDOW);
 	ofRunApp(new testApp()); // start the app
 }
