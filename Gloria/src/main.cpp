bc9a149f9ac06b4a9c1b0029e43ff6bb9949267c
Johan Bichel Lindegaard <johan@johan.cc>
2014-06-26 20:36:52 +0200

    Upgraded to programmable renderer.

--- Gloria/src/main.cpp
+++ Gloria/src/main.cpp
@@ -1,10 +1,12 @@
 #include "ofApp.h"
 #include "ofAppGlutWindow.h"
+#include "ofGlProgrammableRenderer.h"
 
 //--------------------------------------------------------------
 int main(){
-	ofAppGlutWindow window; // create a window
-	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(&window, 1600, 800, OF_WINDOW);
+    
+    ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
+    ofSetupOpenGL(1600, 800, OF_WINDOW);
+    
 	ofRunApp(new ofApp()); // start the app
 }
