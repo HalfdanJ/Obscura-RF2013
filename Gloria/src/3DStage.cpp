cddb5d879ca614f6813a7cd266a63d662a31b8d6
Jonas Jongejan <jonas@halfdanj.dk>
2014-06-30 13:57:28 +0200

    3dstage

---
+++ Gloria/src/3DStage.cpp
@@ -0,0 +1,9 @@
+//
+//  3DStage.cpp
+//  Gloria
+//
+//  Created by Jonas Jongejan on 30/06/14.
+//
+//
+
+#include "3DStage.h"
---
+++ Gloria/src/3DStage.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ContentScene.h"
+#include "mapping.h"
+
